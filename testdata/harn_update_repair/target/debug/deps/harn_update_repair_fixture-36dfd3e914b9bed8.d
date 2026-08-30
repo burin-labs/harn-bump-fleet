c49657cefb9a0263209ae79c9178a6d31c475363
Kenneth Sinder <kennethsinder@gmail.com>
2026-08-30 11:37:04 -0700

    Keep benchmark build output disposable

--- testdata/harn_update_repair/target/debug/deps/harn_update_repair_fixture-36dfd3e914b9bed8.d
+++
@@ -1,9 +0,0 @@
-/Users/ksinder/projects/harn-bump-fleet-worktrees/codex-consumer-privacy/testdata/harn_update_repair/target/debug/deps/harn_update_repair_fixture-36dfd3e914b9bed8.d: src/lib.rs Cargo.toml
-
-/Users/ksinder/projects/harn-bump-fleet-worktrees/codex-consumer-privacy/testdata/harn_update_repair/target/debug/deps/libharn_update_repair_fixture-36dfd3e914b9bed8.rmeta: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
